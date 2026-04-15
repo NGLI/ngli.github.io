6d87fe02a9f5a1545346fe7c4f1ba64006f6c469
shinzoqchiuq <shinzoqchiuq@users.noreply.github.com>
2026-04-15 08:19:32 +0000

    deploy: dac9d8fb0a65a3e9d4bcd3df3372d95bf19e506b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b60f58eb.js',
+                path_to_root + 'searchindex-94a23ba4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
